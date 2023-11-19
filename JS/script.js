9c17f96001a12c65cce0aea460604248edbf1402
mohamedessam77740@gmail.com <mohamedessam77740@gmail.com>
2023-11-19 19:29:08 +0200

    update in code and make code don't reapet

--- JS/script.js
+++ JS/script.js
@@ -3,17 +3,19 @@
 document.querySelector('.guess').value =88;*/
 let num = Math.trunc(Math.floor(Math.random() * 20))+1 ;
 let Score = 20;
-let highscore=0;
+let highscore = 0;
+let DisplayMessage = function (Message) {
+    document.querySelector('.message').textContent = Message;
+}
 document.querySelector('.check').addEventListener('click',function(){
 const guess = Number(document.querySelector('.guess').value); 
 
  //when there is no input
  if(!guess){
-        document.querySelector('.message').textContent ='No Nummber 🙂!';
-        
+     DisplayMessage('No Nummber 🙂!');
     //when user win
- }else if(guess === num){
-        document.querySelector('.message').textContent ='Correct Answer 😍';
+ } else if (guess === num) {
+        DisplayMessage('Correct Answer 😍');
         document.querySelector('body').style.backgroundColor ='rgb(60, 179, 113)';
         document.querySelector('.number').style.width ='30rem';
         document.querySelector('.number').textContent = guess;
@@ -24,33 +26,24 @@ const guess = Number(document.querySelector('.guess').value);
             highscore = Score;
             document.querySelector('.highscore').textContent =highscore + '🤩';
         }
-    //when user input high number
- }else if(guess > num){
+    //when user input wrong number
+ }else if(guess !== num){
     if(Score>1){
-        document.querySelector('.message').textContent ='Too high↗️';
+        document.querySelector('.message').textContent = (guess > num) ? 'Too high↗️' :'Too Low↘️ ';
         document.querySelector('.score').textContent = -- Score ;
 
     }else{
-        document.querySelector('.message').textContent ='You Lost the game 😭';
+        DisplayMessage('You Lost the game 😭');
         document.querySelector('.score').textContent = 0;
         document.querySelector('body').style.backgroundColor ='#DC143C';
     }
-    //when user input Low number
- }else if(guess < num){
-    if(Score>1){
-        document.querySelector('.message').textContent ='Too Low↘️';
-        document.querySelector('.score').textContent = -- Score ;
-    }else{
-        document.querySelector('.message').textContent ='You Lost the game 😭';
-        document.querySelector('.score').textContent = 0;
-        document.querySelector('body').style.backgroundColor ='#DC143C';
-    }
+   
  }
 
 });
 document.querySelector('.again').addEventListener('click',function(){
     Score=20;
-    document.querySelector('.message').textContent ='Start guessing...';
+    DisplayMessage('Start guessing...');
     num = Math.trunc(Math.floor(Math.random() * 20))+1 ;
     document.querySelector('.score').textContent =20;
     document.querySelector('.number').style.width ='15rem';
@@ -59,6 +52,3 @@ document.querySelector('.again').addEventListener('click',function(){
     document.querySelector('.guess').value ='';
     
 });
-
-
-
